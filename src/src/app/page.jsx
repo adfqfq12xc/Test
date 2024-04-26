d8899fca06b7bcfe7c7014a956bd4ecd2c09a2f7
Ali Kansoun <146711317+adfqfq12xc@users.noreply.github.com>
2024-04-26 11:34:08 +0300

    Delete src/src directory

--- src/src/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import Posts from "./posts";
-
-export default function Home() {
-  return (
-    <main style={{  backgroundColor: "#ccc" ,width:"100%",height:"100%"   }}>
-      <Posts />
-    </main>
-  )
-}
